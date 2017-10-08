d1d0e3a4ae1265f1ec1da248a8b9128f87d3a3c3
gagarcr <ggarciaspam@gmail.com>
2017-10-08 12:42:37 +0200

    Changed setup theme functions to functions.php

--- functions.php
+++ functions.php
@@ -25,3 +25,63 @@ function theme_enqueue_styles() {
     wp_enqueue_script( 'child-understrap-scripts', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array(), $the_theme->get( 'Version' ), true );
     wp_enqueue_script( 'mdb_us4-scripts', get_stylesheet_directory_uri() . '/js/mdb_us4.js', array(), false);
 }
+
+
+/**
+ * --------------------------------------------------------------------------
+ * MDB_US4C Custom theme setup
+ * --------------------------------------------------------------------------
+ */
+function mdb_us4c_setup() {    
+    // Set up the Wordpress Theme logo feature.
+    add_theme_support( 'custom-logo', array(
+        'width' => 150,
+        'height' => 150,
+        'flex-width' => true,
+    ) );
+
+    // Add custom-header.php inclusion. This handles the custom header
+    // theme support.
+    add_theme_support( 'custom-header', array(
+        'flex-width'    => true,
+        'width'         => 980,
+        'flex-height'    => true,
+        'height'        => 200,
+        'default-image' => get_template_directory_uri() . '/images/header.jpg',
+    ) );
+
+    // Register new categories filter menu
+    register_nav_menus( array(
+        'category_filter' => 'Catergory Filter Menu'
+    ) );
+}
+
+// Call mdb_us4c_setup
+add_action( 'after_setup_theme', 'mdb_us4c_setup' );
+
+
+/**
+ * --------------------------------------------------------------------------
+ * Helper Functions
+ * --------------------------------------------------------------------------
+ */
+// Custom menu walker. Returns array of elements
+function mdb_us4c_get_menu( $menu_name ) {
+    if ( $menu_name !== '')
+    {
+        if ( ( $locations = get_nav_menu_locations() ) && isset( $locations[ $menu_name ] ) ) {
+            $menu = $locations[ $menu_name ];
+            $menu_items = wp_get_nav_menu_items($menu);
+            return ($menu_items);
+        }
+    }
+   return false; 
+}
+
+?>
+
+<?php
+// TODO: REMOVE
+ini_set('display_errors', 'On');
+error_reporting(E_ALL);
+?>

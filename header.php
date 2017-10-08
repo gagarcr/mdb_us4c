749827294b6e363c0bf76d8c7f64e20c7e3f7282
gagarcr <ggarciaspam@gmail.com>
2017-10-08 12:47:44 +0200

    Initial commit, not yet clean. Only working grid and blur .

--- header.php
+++ header.php
@@ -23,17 +23,26 @@ $container = get_theme_mod( 'understrap_container_type' );
 	<?php wp_head(); ?>
 </head>
 
-<body <?php body_class(); ?>>
+<body <?php body_class(); ?> id="mdb_us4c-body">
+
+<svg class="hideSvgSoThatItSupportsFirefox">
+  <filter id="sharpBlur">
+    <feGaussianBlur stdDeviation="30"></feGaussianBlur>
+    <feColorMatrix type="matrix" values="1 0 0 0 0, 0 1 0 0 0, 0 0 1 0 0, 0 0 0 9 0"></feColorMatrix>
+    <feComposite in2="SourceGraphic" operator="in"></feComposite>
+  </filter>
+</svg>
 
 <style>
 .parallax { 
-   min-height: 200px;
-   height: 60vh; /* 60% of viewport height */
-       margin: 0;
+    min-height: 200px;
+    height: 60vh; /* 60% of viewport height */
+    margin: 0;
     padding: 0;
-   /*max-height: 800px;*/
-overflow: hidden;
-position: relative;
+    /*max-height: 800px;*/
+	overflow: hidden;
+	position: relative;
+	z-index: -1;
 } 
 
 .parallax::before {
@@ -46,11 +55,10 @@ position: relative;
     position: fixed;
     top:0;
     left:0;
-    background: url('<?php echo( get_header_image() );?>') no-repeat bottom center;
+    background: url('<?php echo( get_header_image() );?>') no-repeat top center;
     background-size: cover;
     z-index: -1;
-       will-change: transform; /* creates a new paint layer */
-
+    will-change: transform; /* creates a new paint layer */
 }
 h1 {
 }
@@ -68,17 +76,27 @@ h2.responsive-text{
     margin-bottom: 40px;
     text-transform:  uppercase;
     color: white;
-    text-align: center;
+    //text-align: center;
 }
 .container-fluid{
-    margin: 0;
-    padding: 0;
+    //margin: 0;
+    //padding: 0;
 }
+
+/* Make the content (below the header) non-transparent
+   so that it hides the header when scrolling */
 .wrapper {
     background-color: white;
 }
 
-/* Vertical Align of child objetcs */
+/* Portfolio filter buttons */
+.navbar-filter {
+	background-color: white;
+	margin-top: 0;
+}
+
+/* Vertical Align of child objetcs.
+   DISABLED: Not required anymore in bootstrap4
 .vertical-align {
     display: flex;  
     align-items: center;
@@ -86,31 +104,64 @@ h2.responsive-text{
     height: 100%;
     flex-direction: column;
 }
-
+*/
 .header-button {
-    margin: 20px;
+    //margin: 20px;
 
 }
 /* Navigation bar */
 .navbar {
-    background: white;
+    //background: white;
 }
 .mdb_nav {
-    display: flex;
+    //display: flex;
 }
 </style>
 
-<div id="barba-wrapper">
 
+<div id="barba-wrapper">
 	<?php 
-	function get_current_template() {
-	global $template;
-	return basename($template, '.php');
-	}
+		function get_current_template() {
+		global $template;
+		return basename($template, '.php');
+		}
 	?>
+	<!-- Barba namespace -->
 	<div data-namespace="<?php echo get_current_template() ?>" <?php body_class('barba-container') ?> >
 
 		<div class="hfeed site" id="page">
+			<div class="d-none">
+				<?php if ( 'container' == $container ) : ?>
+					<div class="container">
+				<?php endif; ?>
+
+					<h1 class="title responsive-text mt-1 mr-3 text-right text-uppercase"> 
+						<?php
+							$title = get_bloginfo( 'name', 'raw');
+							$title_words = explode(' ', $title);
+							foreach ($title_words as $value)
+							{
+								echo ($value . ".  ");
+							}
+						?>
+					</h1>
+				<?php if ( 'container' == $container ) : ?>
+					</div><!-- .container -->
+				<?php endif; ?>
+			</div>
+
+
+	
+
+			<div class="container-fluid hidden-sm-block visible-md-block visible-lg-block page-header">
+				<div class="parallax">							
+					
+				</div> <!-- parallax -->
+			</div>
+
+
+
+			<?php if (0): ?>
 
 			<!-- ******************* The Navbar Area ******************* -->
 			<div class="wrapper-fluid wrapper-navbar" id="wrapper-navbar">
@@ -139,10 +190,13 @@ h2.responsive-text{
 								
 							
 							<?php } else {
+								//$custom_logo_id = get_theme_mod( 'custom_logo' );
+								//$logo = wp_get_attachment_image_src( $custom_logo_id , 'thumbnail' );
+								//echo '<img src="'. esc_url( $logo[0] ) .'">';
 								the_custom_logo();
 							} ?><!-- end custom logo -->
 
-						<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+						<button class="navbar-toggler" type="button" data-toggle="collapse-filter" data-target="#navbarNavDropdown-filter" aria-controls="navbarNavDropdown-filter" aria-expanded="false" aria-label="Toggle">
 							<span class="navbar-toggler-icon"></span>
 						</button>
 
@@ -150,8 +204,8 @@ h2.responsive-text{
 						<?php wp_nav_menu(
 							array(
 								'theme_location'  => 'primary',
-								'container_class' => 'collapse navbar-collapse',
-								'container_id'    => 'navbarNavDropdown',
+								'container_class' => 'collapse-filter navbar-collapse',
+								'container_id'    => 'navbarNavDropdown-filter',
 								'menu_class'      => 'navbar-nav',
 								'fallback_cb'     => '',
 								'menu_id'         => 'main-menu',
@@ -165,3 +219,5 @@ h2.responsive-text{
 				</nav><!-- .site-navigation -->
 
 			</div><!-- .wrapper-navbar end -->
+
+			<?php endif; ?>
--- index.php
+++ index.php
@@ -1,3 +1,163 @@
+
+ <style>
+ * {
+	//background-color: rgba(123,120,120,0.2);
+	//border: 1px solid black;
+ }
+
+
+/* Vertical align ( childs, apply to parent ). 
+   Alernative to: "container d-flex h-100 justify-content-center align-items-center" justify-content-center (horizontal, flexbox) align-items-center(vertival, flexbox)*/
+.v-align-child-center {
+	display:flex;
+	height:100%;
+  	align-items:center;
+}
+
+/* Other option for vertical align */
+.v-align-self-center {
+	margin-top: auto;
+	margin-bottom: auto;
+}
+
+/* Horizon align 
+   Alternative to: "justify-content-center" */
+.h-align-self-center {
+	margin-left: auto;
+	margin-right: auto;
+}
+
+/* Custom logo width */
+.custom-logo-link > img {
+	width: 50px;
+	mix-blend-mode: multiply;
+}
+
+/* General style edits */
+/* Remove card border. 
+	We use the body selector to override boostrap4 styles 	
+	#mdb_us4c-body .card 
+	
+	Or 2 selctors ( '.' = and )
+*/
+.card.grid-item {
+	border: none;
+}
+/* Hover projects.
+.card-img-overlay is the overall hover. A .card-img is located below that acts as a background for the hover (that can be blurred)
+*/
+.card.grid-item:hover .overlay {
+        opacity: 1;
+}
+.card.grid-item .card-a {
+	overflow: hidden;
+	background-color: white;
+	position: relative;
+}
+
+.card.grid-item .overlay {
+	position: absolute;
+	top: auto;
+	left: 0;
+	bottom: 0;
+	right: 0;
+
+	height: 5em;
+	opacity: 0;
+	overflow: hidden; 	/* The contents (img) will be bigger, we dont want to show anything */
+
+	background-color: white; /* We set the content with a background, and the content::before with the background blurred. This prevents
+									the edges being transparentens on the blurred background */
+	background-size: cover;       
+	border-radius: calc(.25rem - 1px);
+
+	/* Create a common z-workspace for .overlay childs */
+	z-index: 0;
+
+	/* Required to center the <h1> */
+	display:flex;
+    align-items: center;
+    justify-content: center;
+}
+	
+
+
+.card.grid-item .overlay .card-img {
+	/* We set to absolute and start in the coordinates of the parent. This way we dont distort
+       the image, and it is alligned */
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+
+	opacity: 1;
+
+	transform: translateY(-100%) translateY(+5em) translateZ(0); /* Move 100% up, the 5 em down */
+    //clip: rect(0px,60px,200px,0px);
+	//object-fit: cover;
+	//object-position: bottom;
+
+	 z-index: 1;
+}
+
+.card.grid-item .overlay::before {
+	position: absolute;
+	top: 0;
+	left: 0;
+	bottom: 0;
+	right: 0;
+
+	background-color: rgba(0,0,0,0.4);
+
+	z-index: 2;
+	content: ' ';
+}
+/* Position card-title in the center */
+.card.grid-item .overlay .card-title{
+	position: relative; 
+	margin: 0;
+	//height: 2.5em;
+
+	z-index: 4;
+}
+
+/* transitions */
+.overlay {
+	transition: opacity 0.25s ease;
+}
+
+
+
+
+
+.hideSvgSoThatItSupportsFirefox
+{
+  /* If you use `display: none`, Firefox isn’t able to
+   reference the filter inside the SVG. This avoids
+   the issue by using using .visuallyHidden from the
+   HTML5 Boilerplate.
+   -> https://github.com/h5bp/html5-boilerplate/blob/master/src/css/main.css#L119-L133
+  */
+  border: 0;
+  clip: rect(0 0 0 0);
+  height: 1px;
+  margin: -1px;
+  overflow: hidden;
+  padding: 0;
+  position: absolute;
+  width: 1px;
+}
+
+
+.blur{
+  /* From https://codepen.io/johndjameson/pen/xVjgPy/ */
+  filter: url('#sharpBlur') saturate(200%); /* The radius is defined in <feGaussianBlur stdDeviation="30"> */
+  //filter: blur(30px) saturate(200%);
+}
+  
+ </style>
+
 <?php
 /**
  * The main template file.
@@ -11,7 +171,6 @@
  * @package understrap
  */
 
-get_header();
 
 $container   = get_theme_mod( 'understrap_container_type' );
 $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
@@ -21,18 +180,88 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 	<?php get_template_part( 'global-templates/hero', 'none' ); ?>
 <?php endif; ?>
 
+<?php get_header(); ?>
 
-<!-- Card selector -->
-<div class="wrapper button-group filter-button-group">
-  <button data-filter="*">show all</button>
-  <button data-filter=".col-md-12">col-md-12</button>
-  <button data-filter=".col-md-8">col-md-8</button>
-  <button data-filter=".col-md-4">col-md-4</button>
-</div>
 
 <div class="wrapper" id="wrapper-index">
 
 	<div class="<?php echo esc_attr( $container ); ?>" id="content" tabindex="-1">
+
+
+
+			<!-- ******************* The Filter Navbar Area ******************* -->
+			<div class="wrapper-fluid wrapper-navbar hidden-xl-down" id="wrapper-navbar">
+				<nav class="navbar sticky-top  navbar-expand-md navbar-inverse">
+				<?php if ( 'container' == $container ) : ?>
+					<div class="container">
+				<?php endif; ?>
+						<!-- The WordPress Menu goes here. Only on >sm screen -->
+						<ul id="filter-menu" class="navbar-nav nav-fill w-100 filter-button-group nav-pills"> <!-- Add selector for filter js, nav pills make button-like -->
+
+							<?php		
+								$menu_items = mdb_us4c_get_menu('primary');
+								foreach ($menu_items as $menu_item) {
+									echo ("<li class='d-none d-md-flex align-items-center menu-item menu-item-type-taxonomy menu-item-object-category nav-item text-uppercase'><a class='w-100 nav-link' href='" . $menu_item->url . "' >" . $menu_item->title . "</a></li>");							
+								}
+							?>
+
+							<li class='menu-item menu-item-type-taxonomy menu-item-object-category nav-item '>				
+								<!-- Your site title as branding in the menu -->
+								<?php if ( ! has_custom_logo() ) { ?>
+
+									<?php if ( is_front_page() && is_home() ) : ?>
+
+										<h1 class="navbar-brand mb-0"><a rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a></h1>
+										
+									<?php else : ?>
+
+										<a class="navbar-brand" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>"><?php bloginfo( 'name' ); ?></a>
+									
+									<?php endif; ?>
+								<?php } else { ?>
+									<a class="navbar-brand custom-logo-link d-none d-md-block w-100" rel="home" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>">
+									<?php
+										$custom_logo_id = get_theme_mod( 'custom_logo' );
+										$logo = wp_get_attachment_image_src( $custom_logo_id , 'thumbnail' );
+										echo '<img src="'. esc_url( $logo[0] ) .'"  class="img-fluid" alt="logo" itemprop="logo" >';
+										//the_custom_logo();
+									?>
+									</a>
+								<?php } ?>
+							</li>
+							<!-- end custom logo -->
+							
+							<!-- The filter menu -->
+							<?php		
+								$menu_items = mdb_us4c_get_menu('category_filter');
+								foreach ($menu_items as $menu_item) {
+									echo ("<li class='align-items-center d-flex menu-item menu-item-type-taxonomy menu-item-object-category nav-item text-uppercase'><a class='w-100 nav-link' data-filter='.category-" . $menu_item->title . "' >" . $menu_item->title . "</a></li>");							
+								}
+								
+								// Create filter for isotope
+								$other_filter = ":not(";
+								foreach ($menu_items as $menu_item) {
+									$other_filter .= '.category-' . $menu_item->title . ', ';
+								}
+								// Remove last ',' 
+								$other_filter = rtrim($other_filter, ', ');
+								$other_filter .= ")";
+
+								// Add "Other"
+								echo ("<li class='align-items-center d-flex menu-item menu-item-type-taxonomy menu-item-object-category nav-item text-uppercase'><a class='w-100 nav-link' data-filter='" . $other_filter . "' >Other</a></li>");							
+								
+								
+								
+							?>
+
+						</ul>
+					<?php if ( 'container' == $container ) : ?>
+					</div><!-- .container -->
+					<?php endif; ?>
+
+				</nav><!-- .site-navigation -->
+
+			</div><!-- .filter navbar end -->
 
 		<div class="row">
 
@@ -40,10 +269,12 @@ $sidebar_pos = get_theme_mod( 'understrap_sidebar_position' );
 			<?php get_template_part( 'global-templates/left-sidebar-check', 'none' ); ?>
 
 			<!-- Add grid containter to the main class -->
-			<main class="site-main container" id="main">
-				<div class="row grid">
+			<main class="site-main <?php echo esc_attr( $container ); ?>" id="main">
+				<div class="row grid"> 
+				<!--<div class="card-columns grid">-->
 					<!-- Required for grid library. Add sizing element for columnWidth -->
-					<div class="col-md-4 grid-sizer" hidden></div>
+					<!-- TODO: CHANGE!!! -->
+					<div class="col-md-4 grid-sizer d-none" ></div>
 					<?php if ( have_posts() ) : ?>
 
 						<?php /* Start the Loop */ ?>

cd840e73e3069831a91c10351e04e9163f856f7d
gagarcr <ggarciaspam@gmail.com>
2017-10-08 12:46:57 +0200

    Initial commit after changed to cards layout

--- loop-templates/content.php
+++ loop-templates/content.php
@@ -14,64 +14,41 @@
 	$width = get_post_meta(get_the_ID(), 'portfolio-width', true);
 
 	if ($width == 3):
-		echo('<div class="col-xs-12 col-md-12 grid-item">');
+		$classes = array('col-xs-12 col-md-12 card text-white border-light text-center grid-item p-3');
 	elseif ($width == 2):
-		echo('<div class="col-xs-12 col-md-8 grid-item" >');
+		$classes = array('col-xs-12 col-md-12 card text-white border-light text-center grid-item p-3');
 	else:
-		echo('<div class="col-xs-12 col-md-4 grid-item">');
+		$classes = array('col-xs-12 col-md-4 card text-white border-light text-center grid-item p-3'); // Padding to x-sizer y-sizer
+		//echo('<div class="col-xs-12 col-md-4 grid-item">');			 
 	endif;
+
 ?>
+	<div <?php echo(post_class($classes)); ?>>
+		<!-- Make the hole element clickable -->
+
+		<a class="card-a" href="<?php echo( get_permalink() ) ?>">
+			<img class="card-img" src="<?php echo get_the_post_thumbnail_url( $post->ID, 'large' ); ?>" alt="<?php echo $post->ID ?>">
+			<div class="overlay">
+				<h4 class="card-title text-white text-uppercase"><?php the_title(); ?></h4>
+				<img class="card-img blur" src="<?php echo get_the_post_thumbnail_url( $post->ID, 'large' ); ?>" alt="<?php echo $post->ID ?>">
+				<?php if (0): ?>
+					<p class="card-text"><?php the_excerpt(); ?></p>
+				<?php endif; ?>
+			</div><!-- #card-img-overlay -->
+		
+		
+			<?php if (0): ?>
+			<div class="container-fluid">
+				<div class="row justify-content-center">
+					<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
+				</div><!-- #row -->
+			</div><!-- #container-fluid -->
+			<?php endif; ?>
+			</a><!-- End clickable -->
 
 <article <?php post_class(); ?> id="post-<?php the_ID(); ?>">
-	<!-- Make the hole element clickable -->
-	<a href="<?php echo( get_permalink() ) ?>">
-	<!-- TODO: remove -->
-	<?php if(0): ?>
-	<header class="entry-header">
 
-		<?php the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-		'</a></h2>' ); ?>
-
-		<?php if ( 'post' == get_post_type() ) : ?>
-
-			<div class="entry-meta">
-				<?php understrap_posted_on(); ?>
-			</div><!-- .entry-meta -->
-
-		<?php endif; ?>
-
-	</header><!-- .entry-header -->
-	<!-- TODO: remove -->
-	<?php endif; ?> <!-- If 0 -->
-
-	<?php echo get_the_post_thumbnail( $post->ID, 'large' ); ?>
-	<!-- TODO: remove -->
-	<?php if(0): ?>
-	<div class="entry-content">
-
-		<?php
-		the_excerpt();
-		?>
-
-		<?php
-		wp_link_pages( array(
-			'before' => '<div class="page-links">' . __( 'Pages:', 'understrap' ),
-			'after'  => '</div>',
-		) );
-		?>
-
-	</div><!-- .entry-content -->
-	<!-- TODO: remove -->
-	<?php endif; ?> <!-- If 0 -->
-
-	<footer class="entry-footer">
-		<!-- TODO: remove -->
-		<?php if(0): ?>
-		<?php understrap_entry_footer(); ?>
-		<?php endif; ?> <!-- If 0 -->
-
-
-	</footer><!-- .entry-footer -->
-	</a><!-- End clickable -->
 </article><!-- #post-## -->
+
+
 </div><!-- End custom Tag proccessing --> 

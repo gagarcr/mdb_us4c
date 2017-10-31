ffda58214eabdecf8f182226a747f1b12d6e3b86
gagarcr <ggarciaspam@gmail.com>
2017-11-01 00:10:22 +0100

    Enter

--- footer.php
+++ footer.php
@@ -12,27 +12,24 @@ $container = get_theme_mod( 'understrap_container_type' );
 ?>
 
 <?php if (is_home()): get_sidebar( 'footerfull' ); else: ?>
-  <div class="cross-arrow">
-      <a href="<?php echo get_home_url(); ?>" aria-label="Close">
-        <img class="img-fluid" alt="x" aria-hidden="true" src="<?php echo  get_stylesheet_directory_uri() . '/fonts/ic_close_white_32px.svg'; ?>"></img>
-        <span class="sr-only">&times;</span>			
-      </a>					
-    </div>
-  <?php endif; ?>      
+          <div class="cross-arrow">
+              <a href="<?php echo get_home_url(); ?>" aria-label="Close">
+                <img class="img-fluid" alt="x" aria-hidden="true" src="<?php echo  get_stylesheet_directory_uri() . '/fonts/ic_close_white_32px.svg'; ?>"></img>
+                <span class="sr-only">&times;</span>			
+              </a>					
+            </div>
+<?php endif; ?>      
 
         </div><!-- #page we need this extra closing tag here -->   
-    </div><!-- .barba-contai-ner -->
-</div><!-- #barba-wrapper -->
+    </div><!-- .barba-container -->
+  </div><!-- #barba-wrapper -->
 
-
-<div id="loader-spinner">
-  <i class="fa fa-circle-o-notch fa-spin fa-3x"></i>
-</div>
-
+  <div id="loader-spinner">
+    <i class="fa fa-circle-o-notch fa-spin fa-3x"></i>
+  </div>
 
 <!--Admin bar -->
 <?php wp_footer(); ?>
-
 
 </body>
 

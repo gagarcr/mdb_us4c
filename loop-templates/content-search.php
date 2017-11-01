16cdf47ae8c346b645654b4e7f07a0723fae002c
gagarcr <ggarciaspam@gmail.com>
2017-11-01 03:19:05 +0100

    Layout

--- loop-templates/content-search.php
+++ loop-templates/content-search.php
@@ -9,17 +9,20 @@
             
 <article <?php post_class('card mb-3'); ?> id="post-<?php the_ID(); ?>">
     
-    <header class="entry-header card-header">
-      <?php the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ),
-      '</a>' ); ?>
-    </header><!-- .entry-header -->
+  <header class="entry-header card-header">
+    
+    <?php the_title( sprintf( '<a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a>' ); ?>
+
+  </header><!-- .entry-header -->
     
   <div class="card-body">
+
     <div class="entry-summary card-text">
 
       <?php the_excerpt(); ?>
 
     </div><!-- .entry-summary -->
+    
   </div><!-- .card-body -->
 
 </article><!-- #post-## -->

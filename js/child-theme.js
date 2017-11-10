e48d41121a38dce5a5231b5c82058db5ac70aef5
gagarcr <ggarciaspam@gmail.com>
2017-11-10 19:49:29 +0100

    Automatic compilation

--- js/child-theme.js
+++ js/child-theme.js
@@ -9626,12 +9626,13 @@ var trim = String.prototype.trim ?
 }));
 
 
-
 /*
     Variables
 */
 barbaEnabled = true;
 isotopeEnabled = true;
+mdb_debug = true;
+
 
 /**
  * --------------------------------------------------------------------------
@@ -9689,6 +9690,11 @@ function isotopeStart($) {
             percentPosition: true,
             filter : '*',
             transitionDuration: '.4s',
+            getSortData: {
+              order: '[order] parseInt' // Attribute [order]
+            },
+            sortBy : 'order',
+            //layoutMode: 'packery'
             masonry: {
                 // use element for option
                 columnWidth: '.grid-sizer',
@@ -9696,32 +9702,20 @@ function isotopeStart($) {
             }
         }) // End grid.isotope
 
+        // Each time a image is loaded, call layout
+        grid.imagesLoaded().progress( function() {
+            grid.isotope('layout');
+            console.log("New image loaded .... layout");
+        });
+
         // When all images are loaded, call layout
         grid.imagesLoaded(function(){
-            grid.isotope('layout');
+          console.log("All images loaded");
         });
             
-        console.log("Grid arranged");
+        console.log("Isotope initialized OK");
     }
 }
-
-
-function isotopeLayout($){
-    if (isotopeEnabled) {
-        grid = $('.grid');
-        // Each time a new image is loaded, we layout the grid (progress call)
-        // grid.imagesLoaded()
-        //      .progress(grid.isotope('layout')); // End grid.imagesLoaded()
-        
-        // When all images are loaded (OK, or ERROR), we layout the grid
-        grid.isotope('layout');
-
-        console.log("Grid layout");
-    }
-
-}
-
-
 
 
 /**
@@ -9735,128 +9729,209 @@ if (typeof Barba === 'undefined') {
     throw new Error('Mdb_us4 theme require Barba.js (https://Barba.js.org)');
 }
 
-
 /* Barba Js Views: we can use namespace="page" so that different pages behaves differnty.
 */
-// TODO: move to top on changes
-/*finish: function() {
-    document.body.scrollTop = 0;
-    this.done();
-  }
-  */
 function barbaStart( $ ) {
-//jQuery(document).ready(function( $ ) {
-//document.addEventListener("DOMContentLoaded", function(event) {
-    /* Main page fade */
-    var IndexView = Barba.BaseView.extend({
-        namespace: 'index',
-        onEnter: function() {
-            // The new Container is ready and attached to the DOM.
-            console.log("Index enter");
+  /* Main page fade */
+  var IndexView = Barba.BaseView.extend({
+      namespace: 'index',
+      onEnter: function() {
+        // The new Container is ready and attached to the DOM.
+        console.log("Index enter");
+      },
+      onEnterCompleted: function() {
+          // The Transition has just finished.
 
-
-            try {
-                isotopeStart($);
-                console.log("Isotope initialized from barba");
-            }
-            catch(err) {
-                console.log("Could not initilze isotope: " + err);
-            }
-
-        },
-        onEnterCompleted: function() {
-            // The Transition has just finished.
-            // We call layout each time a new image is loaded
-            // isotopeLayout($);
-
-            // Scroll to index
-            if (typeof ($scrollHomeValue) !== 'undefined')
-            {
-                console.log("Scrooll to previous index: " + $scrollHomeValue);
-                document.body.scrollTop = $scrollHomeValue;
-            }
-        },
-        onLeave: function() {
-            // A new Transition toward a new page has just started.
-            // Store the scroll index 
-            $scrollHomeValue =  document.body.scrollTop;
-        },
-        onLeaveCompleted: function() {
-            // The Container has just been removed from the DOM.
-        }
-    });
-
-    // Don't forget to init the view (index)!
-    IndexView.init();
-
-    // Create a fade transition
-    var FadeTransition = Barba.BaseTransition.extend({
-        start: function() {
-            /**
-             * This function is automatically called as soon the Transition starts
-             * this.newContainerLoading is a Promise for the loading of the new container
-             * (Barba.js also comes with an handy Promise polyfill!)
-             */
-
-            // As soon the loading is finished and the old page is faded out, let's fade the new page
-            Promise
-            .all([this.newContainerLoading, this.fadeOut()])
-            .then(this.fadeIn.bind(this));
-        },
-    
-        fadeOut: function() {
-            /**
-             * this.oldContainer is the HTMLElement of the old Container
-             */
-            console.log("Fade Out");
-            return $(this.oldContainer).animate({ opacity: 0 }).promise();
-        },
-    
-        fadeIn: function() {
-            /**
-             * this.newContainer is the HTMLElement of the new Container
-             * At this stage newContainer is on the DOM (inside our #barba-container and with visibility: hidden)
-             * Please note, newContainer is available just after newContainerLoading is resolved!
-             */
-            console.log("Fade In");
+          try {
+            isotopeStart($);
+            /* TODO:
+            // bind event
+            $grid.isotope( 'on', 'arrangeComplete', function() {
+              console.log('arrange is complete');
+            });*/
+            console.log("Isotope initialized from barba");
             
-            // Scroll to top. TODO: how to store last scroll index
-            document.body.scrollTop = 0;
-
-            var _this = this;
-            var $el = $(this.newContainer);
-
-            $(this.oldContainer).hide();
-
-            $el.css({
-                visibility : 'visible',
-                opacity : 0
-            });
-
-            $el.animate({ opacity: 1 }, 400, function() {
-                /**
-                 * Do not forget to call .done() as soon your transition is finished!
-                 * .done() will automatically remove from the DOM the old Container
-                 */
-        
-                _this.done();
-            });
         }
-    }); /* Base transition extend */
+        catch(err) {
+            console.log("Could not initilze isotope: " + err);
+        }
+
+          // Scroll to index
+          if (typeof ($scrollHomeValue) !== 'undefined')
+          {
+              console.log("Scroll to previous index: " + $scrollHomeValue);
+              window.scrollTo(0, $scrollHomeValue);
+          }
+      },
+      onLeave: function() {
+          // A new Transition toward a new page has just started.
+          // Store the scroll index 
+          $scrollHomeValue = window.scrollY;
+          console.log("Store new scroll index: " + $scrollHomeValue);
+      },
+      onLeaveCompleted: function() {
+          // The Container has just been removed from the DOM.
+      }
+  });
+
+  // Don't forget to init the view (index)!
+  IndexView.init();
+
+  // Create a fade transition
+  var FadeTransition = Barba.BaseTransition.extend({
+    start: function() {
+      /**
+       * This function is automatically called as soon the Transition starts
+       * this.newContainerLoading is a Promise for the loading of the new container
+       * (Barba.js also comes with an handy Promise polyfill!)
+       */
+
+      // As soon the loading is finished and the old page is faded out, 
+      // let's fade the new page.
+      // The .all method creates a new promise that gets resolved as soon as all
+      // promises included as arguments are resolved.
+      Promise
+        //.all([this.newContainerLoading,  this.fadeOut(), this.scrollTop()])
+        //.then(this.fadeIn.bind(this));
+        .all([this.newContainerLoading, this.veilInTransition()])
+        .then(this.scrollTop.bind(this))
+        .then(this.veilOutTransition.bind(this))
+        ;
+    },
+
+    scrollSmoothTop: function() {
+      var deferred = Barba.Utils.deferred();
+      var obj = { y: window.pageYOffset };
+
+      TweenLite.to(obj, 0.4, {
+        y: 0,
+        onUpdate: function() {
+          if (obj.y === 0) {
+            deferred.resolve();
+          }
+
+          window.scroll(0, obj.y);
+        },
+        onComplete: function() {
+          deferred.resolve();
+        }
+      });
+      
+      //deferred.resolve();
+    return deferred.promise;
+    },
+
+    scrollTop: function() {
+      window.scroll(0, 0);
+    },
+
+
     
+    veilInTransition: function() {
+      var p = Barba.Utils.deferred();
+      
+      // Remove any posible modal backdrop
+      $('.modal').modal('hide')
+
+      // Loader fade in transition 
+      var obj = $('#loader-spinner');
+      obj.css({
+        top: 0,
+        opacity: 1,
+      });   
+
+      // Fallback in case transitionend is not suported 
+      var fallbackTimeout = setTimeout(function (){
+        obj.removeAttr('style');
+        console.log("transitionend fallback");
+        p.resolve();
+      }, 2000);
+
+      // Wait for css transition to finish 
+      obj.one('transitionend', function (e) {
+        clearTimeout(fallbackTimeout);            
+        console.log("Fade in complete");
+        p.resolve();
+      });
+
+      // JQuery animation 
+      /*
+      $(this.oldContainer).animate({ opacity: 0 }, 1000, function (){
+        console.log("Fade in complete");
+        p.resolve();
+        
+      });*/
+      
+      return p.promise;
+    },
+
+    veilOutTransition: function() {
+      var p = Barba.Utils.deferred();
+      
+      console.log("Veil out");
+
+      var _this = this;
+      var $el = $(this.newContainer);
+      var obj = $('#loader-spinner');
+      
+      // Destroy old container
+      _this.done();
+
+      // New container visible
+      $el.css({
+        visibility : 'visible',
+      });   
+      
+      // Start loader fade transition
+      obj.css({
+        opacity: 0,
+      });   
+
+      // Fallback in case transitionend is not suported
+      var fallbackTimeout = setTimeout(function (){
+        obj.removeAttr('style');
+        console.log("transitionend fallback");
+        p.resolve();
+      }, 2000);
+        
+      // Wait for css transition to finish
+      obj.one('transitionend', function (e) {           
+        obj.removeAttr('style');            
+        clearTimeout(fallbackTimeout);
+        console.log("Fade out complete");
+        p.resolve();
+      });
+
+
+      //setTimeout(function (){
+        /* JQuery animation */
+        //$el.animate({ opacity: 1 }, 1300, function() {
+          /**
+           * Do not forget to call .done() as soon your transition is finished!
+           * .done() will automatically remove from the DOM the old Container
+           */
+    
+        //  console.log("Veil out complete");
+          // p.resolve();            
+        //});
+      //}, 8000);
+
+      return p.promise;
+    },
+  }); /* Base transition extend */
+  
+  /**
+   * Next step, you have to tell Barba to use the new Transition
+   */
+
+  Barba.Pjax.getTransition = function() {
     /**
-     * Next step, you have to tell Barba to use the new Transition
+     * Here you can use your own logic!
+     * For example you can use different Transition based on the current page or link...
      */
 
-    Barba.Pjax.getTransition = function() {
-        /**
-         * Here you can use your own logic!
-         * For example you can use different Transition based on the current page or link...
-         */
-
-        return FadeTransition;
-    };
-//}); /* JQuery ready */
+    return FadeTransition;
+  };
 };
 
 /**
@@ -9874,12 +9949,15 @@ jQuery(document).ready(function( $ ) {
         // In this way Pjax.start() will emit onEnter() and onEnterCompleted() for the current view.
         try {
             barbaStart($);
-                 
-            console.log("Barba prefetch init");
 
             Barba.Pjax.start();
+
+            // Start prefecth 
+            console.log("Barba prefetch init");
             Barba.Prefetch.init();
-            console.log("Barba start");
+
+            // Call common JS initialization functions
+            mdb_us4_initFunctions( $ );            
         }
         catch(err) {
             console.log("Could not initialize Barba: " + err);
@@ -9892,22 +9970,43 @@ jQuery(document).ready(function( $ ) {
         mdb_us4_fallback( $ );
        
     }
+
 }); // End jQuery
     
 
+// Each time a new page is loaded by PJAX, show a log
+Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {    
+    var $ = jQuery;
+    
+    if (mdb_debug) {
+      // Gulp does not convert ajax links correctly
+      if ((window.location.href.includes("localhost")) || (window.location.origin.includes("192.168.")) ){   
+        $('body a[href*="vccw.dev"]').each(function() {
+          $(this).attr('href', $(this).attr('href').replace('vccw.dev', window.location.origin.replace("http://", "")));        
+        });
+      }
+    }
+
+    mdb_us4_initFunctions( $ );            
+});
+
+
 /**
  * --------------------------------------------------------------------------
- * DEBUG
+ * Common funcions
  * --------------------------------------------------------------------------
  */
 
-// Each time a new page is loaded by PJAX, show a log
-Barba.Dispatcher.on('newPageReady', function(currentStatus, oldStatus, container) {    
-    //console.log("New page ready!");      
-    mdb_us4_scrollSmooth( jQuery );
-    
-});
+// Common JS initialization functions.
+// Each time a new page is loaded, this should be called
+function mdb_us4_initFunctions( $ ){
 
+  // Start smooth scroll button (ex: single post arrow)
+  mdb_us4_scrollSmooth( $ );
+   
+  // Start tooltips
+  $('[data-toggle="tooltip"]').tooltip();    
+};
 
 // Fallback function that loads everithing done by Barba.js.
 function mdb_us4_fallback ( $ ) {
@@ -9920,50 +10019,28 @@ function mdb_us4_fallback ( $ ) {
         console.log("Could not initialize isotope: " + err);
     }
 
-    // Start smooth scroll button (ex: single post arrow)
-    mdb_us4_scrollSmooth( $ );
+    mdb_us4_initFunctions($);
 }; 
 
-
-
-
+// Initialize the scroll down arrow behavior
 function mdb_us4_scrollSmooth( $ ) {
     console.log("ScrollSmooth load");
+
     $('.scroll-arrow').click(function( event) {
         console.log("Scroll smooth");
         var sectionTo = $(this).attr('href');
-
-
         //sectionTo.animatedScroll({easing: "easeOutExpo"});
-        
         $('html, body').animate({
             scrollTop: $(sectionTo).offset().top
          }, 1000);
          event.preventDefault();
     });
-
 };
 
 
 
-( function( $ ) {
-    
-    
-
-
-
-  $( function() { // Ready
-  console.log("this thing loads jquery!");
-
-  
-  } );
-  
-})( jQuery );
-
-
-
-jQuery(document).ready(function( $ ) {
-
-  $('[data-toggle="tooltip"]').tooltip()    
-  
-});  
+/**
+ * --------------------------------------------------------------------------
+ * DEBUG
+ * --------------------------------------------------------------------------
+ */
